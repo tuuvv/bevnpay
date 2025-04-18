5998083397d057beccb9b93a6b0c80b0f2f78856
tuuvv <43124292+tuuvv@users.noreply.github.com>
2025-04-19 00:17:48 +0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,7 @@ module.exports = {
       script: 'dist/main.js',
       cwd: './',
       instances: 1,
+      exec_mode: 'fork', 
       autorestart: true,
       watch: false,
       max_memory_restart: '500M',
