7c43e2248024a850b81b2dccdb3e868d7a2a05ac
tuuvv <43124292+tuuvv@users.noreply.github.com>
2025-04-19 15:39:34 +0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,13 +5,14 @@ module.exports = {
       script: 'dist/main.js',
       cwd: './',
       instances: 1,
+      exec_mode: 'fork', 
       autorestart: true,
       watch: false,
       max_memory_restart: '500M',
       env: {
         NODE_ENV: 'production',
         PORT: 4000,
-        MONGO_URI: 'mongodb://localhost:27017/ecommerce'
+        MONGO_URI: 'mongodb://127.0.0.1:27017/ecommerce'
       }
     }
   ]
